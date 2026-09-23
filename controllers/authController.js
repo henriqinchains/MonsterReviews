162b14609da471069662007680fee8864487d572
Francis <gui.mad23@gmail.com>
2026-09-23 20:40:49 -0300

    nunca mais deixe o frontend tocar nessa pérola

--- controllers/authController.js
+++ controllers/authController.js
@@ -161,10 +161,10 @@ exports.verify2FA = async (req, res) => {
 
     res.cookie("authToken", tokenReal, { httpOnly: true, secure: true, sameSite: "none", partitioned: true, maxAge: 7 * 24 * 60 * 60 * 1000 });
 
-    // 🛡️ NOVIDADE: Se o usuário marcou a caixinha, gera o cookie VIP de 30 dias
+    // 🛡️ NOVIDADE: Se o usuário marcou a caixinha, gera o cookie VIP de 180 dias
     if (salvarDispositivo) {
-      const trustedToken = jwt.sign({ id: usuarioFinal._id }, process.env.JWT_SECRET, { expiresIn: "30d" });
-      res.cookie("trustedDevice", trustedToken, { httpOnly: true, secure: true, sameSite: "none", partitioned: true, maxAge: 30 * 24 * 60 * 60 * 1000 });
+      const trustedToken = jwt.sign({ id: usuarioFinal._id }, process.env.JWT_SECRET, { expiresIn: "180d" });
+      res.cookie("trustedDevice", trustedToken, { httpOnly: true, secure: true, sameSite: "none", partitioned: true, maxAge: 180 * 24 * 60 * 60 * 1000 });
     }
 
     return res.status(200).json({
